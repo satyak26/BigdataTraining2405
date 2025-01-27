b595fff49e50d2a90c23e4210567d2dce845eac8
satyak26 <109522407+satyak26@users.noreply.github.com>
2025-01-27 15:52:37 -0600

    Title Output

--- Titlecode.py
+++ Titlecode.py
@@ -1,13 +1,10 @@
 from bs4 import BeautifulSoup
 
-# Open and read the HTML file
-with open("BestbuyHTML.txt", "r", encoding="utf-8") as file:
+with open("/mnt/c/Users/satya/Downloads/BestbuyHTML.txt", "r", encoding="utf-8") as file:
     content = file.read()
 
-# Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(content, "html.parser")
 
-# Extract the title tag
 title_tag = soup.title
 
 if title_tag:
